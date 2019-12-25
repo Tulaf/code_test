3f61f1446399f56f17689858fd7040f8691f4864
Tulaf <liuxiaohan.cool@163.com>
2019-12-25 16:32:20 +0800

    decimal to binary

---
+++ binary.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void to_binary(unsigned long n);
+
+int main(void){
+    unsigned long number;
+    printf("Enter a integer (q to quit):");
+    while (scanf("%lu",&number)==1){
+        printf("Binary equivalent: ");
+        to_binary(number);
+        putchar('\n');
+        printf("Enter a integer (q to quit):");
+    }
+    printf("Done.\n");
+    return 0;
+}
+void to_binary(unsigned long n){
+    int r;
+    r=n%2;
+    if(n>=2) to_binary(n/2);
+    putchar(r==0?'0':'1');
+}
